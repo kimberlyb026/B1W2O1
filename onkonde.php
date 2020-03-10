c28624a6c843dffe0d6c4c54c1e9816ad84fdd29
kimberly <99051407@mydavinci.nl>
2020-03-10 09:09:00 +0100

    2

--- onkonde.php
+++ onkonde.php
@@ -11,14 +11,41 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="paniek.css">
+<link href="https://fonts.googleapis.com/css?family=Sonsie+One&display=swap" rel="stylesheet">
 <title>paniek</title>
 </head>
+<h1>mad libs</h1>
 
-<div class= verhaal>
+  <nav>
+    <ul>
+          <li><a href="paniek.html">Er heerst paniek...</a></li>
+          <li><a href="onkonde.html">onkonde</a></li>
+         
+        
+    </ul>
+
+
+
+  </nav>
+
+<div class="grid-container">
+	   <div id= "verhaal1">
+  <h2>Onkonde</h2>
+<div id= verhaal>
 <p> Er zijn veel mensen die niet kunnen <?= $_POST['vraag1']?>. Neem nou <?= $_POST['vraag2']?>. Zelfs met de hulp van een <?= $_POST['vraag3']?> of zelfs <?= $_POST['vraag4']?> kan <?= $_POST['vraag2']?> niet <?= $_POST['vraag1']?>. Dat heeft niet te maken met een gebrek aan <?= $_POST['vraag5']?>, maar met een te veel aan <?= $_POST['vraag6']?>. Teveel <?= $_POST['vraag6']?> leidt tot <?= $_POST['vraag7']?> en dat is niet goed als je wilt <?= $_POST['vraag1']?>. Helaas voor <?= $_POST['vraag2']?>
 
-
+</p>
 </div>
+<div class="footer">
+	<footer>
+  <p>deze website is gemaakt door: Kimberly van Berkum</p>
+  </footer>
+</div>
+</div>
+
+
+
+
 </body>
 </html>
 
--- paniek.php
+++ paniek.php
@@ -10,10 +10,29 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="paniek.css">
+<link href="https://fonts.googleapis.com/css?family=Sonsie+One&display=swap" rel="stylesheet">
 <title>paniek</title>
 </head>
 
-<div class= verhaal>
+<h1>mad libs</h1>
+<div class="nav">
+  <nav>
+    <ul>
+           <li><a href="paniek.html">Er heerst paniek...</a></li>
+          <li><a href="onkonde.html">onkonde</a></li>
+         
+        
+    </ul>
+
+  </nav>
+</div>
+
+
+  <div class="grid-container">
+    <div id= "verhaal1">
+      <h2>Er heerst paniek...</h2>
+
+<div id= verhaal>
 <p> Er heerst paniek in het koninkrijk <?= $_POST['vraag3']?>  Koning <?= $_POST['vraag6']?> is ten einde raad en als koning <?= $_POST['vraag6']?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?= $_POST['vraag2']?></p>
 <p>  "<?= $_POST['vraag2']?>! Het is een ramp! Het is een schande!" </p>
 <p>"Sire,Majesteit, Uwe luidruchtigheid, wat is er aan de hand?" </p>
@@ -22,11 +41,16 @@
 <p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?= $_POST['vraag8']?> leren?"</p>
 <p>"Maar Sire, daar kunt u toch uw <?= $_POST['vraag7']?> voor gebruiken"</p>
 <p>" <?= $_POST['vraag2']?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had"</p>
-<p>" Mij <?= $_POST['vraag4']?>, Sire"</p>
+<p>"<?= $_POST['vraag4']?>, Sire"</p>
 
 
 
 </div>
+<footer>
+  <p>deze website is gemaakt door: Kimberly van Berkum</p>
+  </footer>
+</div>
+
 
 </body>
 </html>
